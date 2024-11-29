1b9b6f49f0624027dbfc76dbf8730d4106cb15c1
Jason <thatstealthyowl@gmail.com>
2024-11-29 16:52:48 -0600

    update cpu binary parse

--- belle/src/hardware/cpu.rs
+++ belle/src/hardware/cpu.rs
@@ -35,9 +35,15 @@ impl CPU {
         }
     }
     pub fn load_binary(&mut self, binary: Vec<i16>) {
+        let mut in_subr = false;
         let mut counter = 0;
         let mut start_found = false;
         for element in binary {
+            if in_subr && (element >> 12) != RET_OP {
+                continue;
+            } else if (element >> 12) == RET_OP {
+                in_subr = false;
+            }
             if (element >> 9) == 1 {
                 if start_found {
                     EmuError::Duplicate(".start directives".to_string()).err();
@@ -51,16 +57,27 @@ impl CPU {
                 }
                 continue;
             }
-            if (element >> 12) != 0b1111 {
-                self.memory[counter] = Some(element);
+            println!("{:b}", (element >> 12) & 0b0000000000001111u16 as i16);
+            if (element >> 12) & 0b0000000000001111u16 as i16 != 0b1111 {
+                self.memory[counter + self.starts_at as usize] = Some(element);
                 if CONFIG.verbose {
                     println!("Element {:016b} loaded into memory", element);
                 }
+            } else {
+                in_subr = true;
+                continue;
             }
             counter += 1;
         }
+        let mut some_count = 0;
+        for i in 0..=65535 {
+            if self.memory[i as usize].is_some() {
+                some_count += 1;
+            }
+        }
+        println!("Some count: {some_count}");
     }
- fn shift_memory(&mut self) {
+    fn shift_memory(&mut self) {
         let mut some_count = 0;
         if CONFIG.verbose {
             println!("Shifting memory...");
@@ -70,18 +87,20 @@ impl CPU {
                 some_count += 1;
             }
         }
-            // check for overflow
+        // check for overflow
         if some_count + self.starts_at > 65535 {
             EmuError::MemoryOverflow().err();
         }
         let mem_copy = self.memory;
         self.memory = [None; 65536];
-        for i in 0.=65536 {
-            self.memory[(i + self.starts_at) as usize] = mem_copy[i as usize];
+        for i in 0..=65535 {
+            if mem_copy[i as usize].is_some() {
+                self.memory[(i + self.starts_at) as usize] = mem_copy[i as usize];
+            }
         }
         if CONFIG.verbose {
             println!("Shift completed.");
-            println!("Memory: {:?}", self.memory);
+            //    println!("Memory: {:?}", self.memory);
         }
     }
 }
