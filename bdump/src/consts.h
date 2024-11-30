c0c9135f4cbf9bf3f10966bae69f8e9fd781b7f1
gummi <thatstealthyowl@gmail.com>
2024-11-30 15:29:44 -0600

    fixed consts

--- bdump/src/consts.h
+++ bdump/src/consts.h
@@ -1,37 +1,35 @@
 #ifndef CONSTS_H
 #define CONSTS_H
-
-#define HLT_OP 0b0000 // we need this
-#define ADD_OP 0b0001 // we also need this
-#define JGE_OP 0b0010 // maybe optional ?
-#define CL_OP 0b0011  // maybe optional ?
-#define DIV_OP 0b0100 // we need this
-#define RET_OP 0b0101 // we need this
-#define LD_OP 0b0110  // we need this
-#define ST_OP 0b0111  // we need this
-#define SWP_OP 0b1000 // we need this
-#define JZ_OP 0b1001 // maybe optional ?
-#define CMP_OP 0b1010 // we need this
-#define MUL_OP 0b1011 // we need this
-#define SET_OP 0b1100 // we need this
-#define INT_OP 0b1101 // we need this
-#define MOV_OP 0b1110 // we need this
-#define SR_OP 0b1111  // we need this
-
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#define HLT_OP 0b0000 
+#define ADD_OP 0b0001 
+#define JGE_OP 0b0010 
+#define CL_OP 0b0011  
+#define DIV_OP 0b0100 
+#define RET_OP 0b0101 
+#define LD_OP 0b0110  
+#define ST_OP 0b0111  
+#define SWP_OP 0b1000 
+#define JZ_OP 0b1001  
+#define CMP_OP 0b1010 
+#define MUL_OP 0b1011 
+#define SET_OP 0b1100 
+#define INT_OP 0b1101 
+#define MOV_OP 0b1110 
+#define SR_OP 0b1111  
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 static bool supports_ansi() {
     const char *term = getenv("TERM");
     return term && (strstr(term, "xterm") || strstr(term, "screen") || strstr(term, "linux"));
 }
-
-#if defined(ENABLE_ANSI) && supports_ansi()
+#ifdef ENABLE_ANSI
     #define ANSI_RESET "\033[0m"
     #define ANSI_BOLD "\033[1m"
     #define ANSI_UNDERLINE "\033[4m"
-
     #define ANSI_BLACK "\033[30m"
     #define ANSI_RED "\033[31m"
     #define ANSI_GREEN "\033[32m"
@@ -42,7 +40,6 @@ static bool supports_ansi() {
     #define ANSI_WHITE "\033[37m"
     #define ANSI_GRAY "\033[90m"
     #define ANSI_LIGHT_GRAY "\033[37m"
-
     #define ANSI_BG_BLACK "\033[40m"
     #define ANSI_BG_RED "\033[41m"
     #define ANSI_BG_GREEN "\033[42m"
@@ -55,7 +52,6 @@ static bool supports_ansi() {
     #define ANSI_RESET ""
     #define ANSI_BOLD ""
     #define ANSI_UNDERLINE ""
-
     #define ANSI_BLACK ""
     #define ANSI_RED ""
     #define ANSI_GREEN ""
@@ -66,7 +62,6 @@ static bool supports_ansi() {
     #define ANSI_WHITE ""
     #define ANSI_GRAY ""
     #define ANSI_LIGHT_GRAY ""
-
     #define ANSI_BG_BLACK ""
     #define ANSI_BG_RED ""
     #define ANSI_BG_GREEN ""
@@ -76,9 +71,7 @@ static bool supports_ansi() {
     #define ANSI_BG_CYAN ""
     #define ANSI_BG_WHITE ""
 #endif
-
 #ifdef __cplusplus
 }
 #endif
-
 #endif
