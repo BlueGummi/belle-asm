1165b6cee663229aab3819d477303f89adf946d7
gummi <thatstealthyowl@gmail.com>
2024-12-02 17:20:44 -0600

    btils - BELLE Utils

--- examples/fib.asm
+++ examples/fib.asm
@@ -1,26 +1,26 @@
-	; $1 is x
-	; $2 is y
+				
+				
 .sbp $100
 .ssp $1
 .start $500
-	int #15  ; halt on overflow
-	mov %r0, #0 ; x
-	mov %r1, #1 ; y
-	push %r0
-    push %r1
-	int #11
-	jz @loop
+				
+				
+				
+						push %r0
+					    	push %r1
+						int #11
+						jz @loop
 loop:
-	mov %r2, #0 ; z
-	pop %r1 ; load them back
-	pop %r0
-	add %r2, %r1 ; z = z + y
-	add %r2, %r0 ; z = z + x
-	mov %r0, %r1 ; x = y
-	mov %r1, %r2 ; y = z
-	push %r0 ; put them back
-	push %r1
-	int #2
-	int #11
-	jz @loop
-
+				
+				
+						pop %r0
+				
+				
+				
+				
+				
+						push %r1
+						int #2
+						int #11
+						jz @loop
+					
--- examples/primes.asm
+++ examples/primes.asm
@@ -1,29 +1,29 @@
-.start $50 ; 50 spots more than enough
-	; r1 will contain the number we are checking
-	; r0 will be the number we are dividing by
-	set #1
-	mov %r1, #2
-	int #1
-	jz @prime_check_loop
-prime_check_loop:
-	add %r1, #1
-	set #1
-	mov %r0, #2
-	jz @plop
-	set #1
-	jz @prime_check_loop
-	ret
-plop:
-	mov %r2, %r1
-	div %r2, %r0
-	int #14 ; zflag = rflag
-	int #11 ; flip it
-	jz @end_print
-	add %r0, #1
-	set #1
-	jz @plop
-	ret
-end_print:
-	int #1
-	ret
 
+
+
+		set #1
+		mov %r1, #2
+		int #1
+		jz @prime_check_loop
+prime_check_loop:
+		add %r1, #1
+		set #1
+		mov %r0, #2
+		jz @plop
+		set #1
+		jz @prime_check_loop
+		ret
+plop:
+		mov %r2, %r1
+		div %r2, %r0
+
+
+		jz @end_print
+		add %r0, #1
+		set #1
+		jz @plop
+		ret
+end_print:
+		int #1
+		ret
+	
