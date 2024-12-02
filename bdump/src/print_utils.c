0caedc93f6a6d6cc8a7c30d4ed466c0fca00cd8d
gummi <thatstealthyowl@gmail.com>
2024-12-02 14:56:46 -0600

    fix makefile and bdump

--- bdump/src/print_utils.c
+++ bdump/src/print_utils.c
@@ -89,15 +89,14 @@ void print_hlt_instruction(Instruction *ins, bool colors) {
             printf("%s.ssp%s%s $%d%s\n", ANSI_GREEN, ANSI_RESET, ANSI_YELLOW, ins->type,
                    ANSI_RESET);
         } else {
-            printf(".sbp $%d\n", ins->type);
+            printf(".ssp $%d\n", ins->type);
         }
     } else if (ins->destination == 3) {
-        ins->type = (ins->type << 8) | ins->source;
         if (colors) {
-            printf("%s.sbp%s%s $%d%s\n", ANSI_GREEN, ANSI_RESET, ANSI_YELLOW, ins->type,
+            printf("%s.sbp%s%s $%d%s\n", ANSI_GREEN, ANSI_RESET, ANSI_YELLOW, ins->source,
                    ANSI_RESET);
         } else {
-            printf(".sbp $%d\n", ins->type);
+            printf(".sbp $%d\n", ins->source);
         }
     } else {
         if (colors) {
@@ -180,7 +179,7 @@ void print_output(Instruction *ins) {
             printf("$%d, %%r%d\n", ins->destination, ins->source);
         }
     } else if (strcmp(op, "push") == 0 || strcmp(op, "pop") == 0) {
-        if (type == 0) {
+        if (ins->type == 0) {
             if (colors) {
                 printf("%s%%%d%s\n", ANSI_YELLOW, ins->source, ANSI_RESET);
             } else {
