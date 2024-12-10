cefe12b7ce59c49c5268ea7402b085255f419158
gummi <thatstealthyowl@gmail.com>
2024-12-10 14:23:59 -0600

    fibonacci using jo

--- examples/fib.asm
+++ examples/fib.asm
@@ -1,26 +1,28 @@
-; This program calculates the fibonacci sequence to the largest signed 16 bit integer value
+    ; This program calculates the fibonacci sequence to the largest signed 16 bit integer value
     ; $1 is x
-	; $2 is y
+    ; $2 is y
 .sbp $10
 .ssp $1
 .start $100
-	int #51             ; halt on overflow
-	mov %r0, #0         ; first value of fibonacci
-	mov %r1, #1         ; second value of fibonacci
-	push %r0            ; r0 onto stack
-	push %r1            ; r1 onto stack 
-	int #11             
-	jz $108             
+    mov %r0, #0         ; first value of fibonacci
+    mov %r1, #1         ; second value of fibonacci
+    push %r0            ; r0 onto stack
+    push %r1            ; r1 onto stack 
+    int #11             
+    jz @fib_loop
+fib_loop:
     pop %r4             ; pop the jump address and save it
-	mov %r2, #0         ; final value of fibonacci
-	pop %r1             ; load values back into registers
-	pop %r0
-	add %r2, %r1        ; z += y
-	add %r2, %r0        ; z += x
-	mov %r0, %r1        ; x = y
-	mov %r1, %r2        ; y = z
-	push %r0            ; push values back onto stack
-	push %r1
-	int #2              ; print the value in register 2
-	jz $108             ; continue loop
-
+    mov %r2, #0         ; final value of fibonacci
+    pop %r1             ; load values back into registers
+    pop %r0
+    add %r2, %r1        ; z += y
+    add %r2, %r0        ; z += x
+    mov %r0, %r1        ; x = y
+    mov %r1, %r2        ; y = z
+    push %r0            ; push values back onto stack
+    push %r1
+    jo @finish
+    int #2              ; print the value in register 2
+    jz @fib_loop        ; continue loop
+finish:
+    hlt
